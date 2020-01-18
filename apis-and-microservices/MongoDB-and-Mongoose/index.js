e3bf0b1c31db61572d063070b2f0bfafbcfaf5d2
Игорь Яшкин <injashkin@gmail.com>
2020-01-18 23:19:54 +0300

    editing MongoDB-and-Mongoose.md

--- apis-and-microservices/MongoDB-and-Mongoose/index.js
+++ apis-and-microservices/MongoDB-and-Mongoose/index.js
@@ -7,13 +7,14 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 });
 
 var userSchema = new mongoose.Schema({
+  _id: { type: Number },
   name: { type: String, default: "Анонимный" },
   age: { type: Number, min: 18, index: true }
 });
 
 const UserModel = mongoose.model("UserModel", userSchema);
 
-var ivanPetrov = new UserModel({ name: "Ivan Petrov", age: 25, date: 12 - 07 - 2019 });
+var ivanPetrov = new UserModel({ _id: 1, name: "Ivan Petrov", age: 25, date: 12 - 07 - 2019 });
 
 ivanPetrov.save(function (err) {
   if (err) return console.error(err);
@@ -21,9 +22,9 @@ ivanPetrov.save(function (err) {
 });
 
 var arrayUsers = [
-  { name: "Светлана", age: 21 },
-  { name: "Kamila", age: 35 },
-  { name: "Олег", age: 27 }
+  { _id: 2, name: "Светлана", age: 21 },
+  { _id: 3, name: "Kamila", age: 35 },
+  { _id: 4, name: "Олег", age: 27 }
 ];
 
 UserModel.create(arrayUsers, function (err) {
@@ -31,7 +32,19 @@ UserModel.create(arrayUsers, function (err) {
   console.log('Пользователи созданы');
 });
 
-UserModel.find({name: "Светлана"}, function (err, userFound) {
+UserModel.find({ name: "Светлана" }, function (err, userFound) {
   if (err) return console.log(err);
   console.log('Пользователь' + userFound + 'найден');
+});
+
+UserModel.findOne({ name: "Светлана" }, function (err, data) {
+  if (err) return console.log(err);
+  console.log('Пользователь' + data + 'найден');
+});
+
+userId = 4
+UserModel.findById(userId, function (err, dat) {
+  if (err) return console.log(err);
+  console.log('Пользователь c _id = ' + userId + ' найден');
+  console.log(dat);
 });
