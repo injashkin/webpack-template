5d0a907362d6087e1a5394766a09deb9265f9d6d
injashkin <injashkin@gmail.com>
2022-09-28 23:28:01 +0300

    Add babel and refactor code

---
+++ dist/index.js
@@ -0,0 +1,1 @@
+(()=>{var e;document.body.prepend(("Проект собран на Webpack",(e=document.createElement("h1")).textContent="Проект собран на Webpack",e))})();
--- webpack.config.js
+++ webpack.config.js
@@ -2,22 +2,32 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
+  entry: path.join(__dirname, 'src', 'index.js'),
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'index.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'src', 'index.pug'),
+      filename: 'index.html',
+    }),
+  ],
+  devServer: {
+    watchFiles: path.join(__dirname, 'src'),
+    port: 9000,
+  },
   module: {
     rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
       {
         test: /\.pug$/,
         loader: 'pug-loader',
       },
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.pug',
-      filename: 'index.html',
-    }),
-  ],
-  devServer: {
-    watchFiles: ['./src', './images'],
-    port: 9000,
   },
 };
