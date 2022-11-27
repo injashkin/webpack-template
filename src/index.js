04617e4b851ca5d7c17e2f7cdaf4381f2f357b0f
injashkin <injashkin@gmail.com>
2022-11-27 23:48:15 +0300

    Add React

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app';
 import './main.scss';
 import './fonts/fonts.scss';
 
@@ -8,3 +11,5 @@ function component(text) {
 }
 
 document.body.prepend(component('Проект собран на Webpack'));
+
+ReactDOM.render(<App />, document.getElementById('root'));
